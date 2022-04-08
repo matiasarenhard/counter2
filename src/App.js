2fdcccd96d2d11b380cbdf7f5d26c6896b457ede
Matias <matias_arenhard@hotmail.com>
2022-04-07 23:20:38 -0300

    child component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
+import Child from "./components/Child";
+
 function App() {
-  return <>Hellow</>
+  const [counter, setCounter] = useState(0);
+  return (<main>
+    <h1>Father component: {counter}</h1>
+    <button onClick={() => setCounter(counter + 1)}>+1</button>
+    <Child />
+  </main>);
 }
 
 export default App;
---
+++ src/components/Child/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Child = () => {
+    return (
+        <div className="child">
+            <h2>Son component</h2>
+        </div>
+    );
+};
+
+export default Child;
