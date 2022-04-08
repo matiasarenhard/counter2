80f75c9002f680b5eb581b98e5235be247dc98aa
Matias <matias_arenhard@hotmail.com>
2022-04-07 23:52:25 -0300

    learn react memo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Child from "./components/Child";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  return (<main>
-    <h1>Father component: {counter}</h1>
-    <button onClick={() => setCounter(counter + 1)}>+1</button>
-    <Child />
-  </main>);
+  const [input, setInput] = useState("");
+  return (
+    <main>
+      <h1>Father component: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <br />
+      <br />
+      <input type="text" value={input}
+        onChange={({target}) => setInput(target.value)}
+      />
+      <Child counter={counter}/>
+    </main>
+  );
 }
 
 export default App;
--- src/components/Child/index.js
+++ src/components/Child/index.js
@@ -1,11 +1,13 @@
 import "./styles.css";
+import { memo } from "react";
 
-const Child = () => {
+
+const Child = ({ counter }) => {
     return (
         <div className="child">
-            <h2>Son component</h2>
+            <h2>Son component: { counter }</h2>
         </div>
     );
 };
 
-export default Child;
+export default memo(Child);
